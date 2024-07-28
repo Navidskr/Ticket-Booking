120710a669eb433b408a4855bf6d13829316b469
Navid <saghi.navid8@gmail.com>
2024-07-28 13:35:08 +0430

    last step bug fixing

--- source/script/ticket.js
+++ source/script/ticket.js
@@ -6,13 +6,25 @@ let countriesData = {
     Turkey :['Istanbul','Izmir','Ankara','Antalia'],
     USA : ['NewYork','LosAngles','SanDiego','Texas']
 }
+
+
 countrySelectBox.addEventListener('change', function(){
-    let mainCountryName = countrySelectBox.value
-    let mainCountryCities = countriesData[mainCountryName]
 
-    mainCountryCities.forEach(function(city){
-        // console.log(city)
-        citySelect.innerHTML += '<option>' + city + '</option>'
+    if(countrySelectBox.value === 'Please Select'){
+        citySelect.innerHTML = ""
+        citySelect.innerHTML += '<option>Select City</option>'
+    }else{
+        let mainCountryName = countrySelectBox.value
+        let mainCountryCities = countriesData[mainCountryName]
+    
+        citySelect.innerHTML = ""
+    
+        mainCountryCities.forEach(function(city){
+    
+            // console.log(city)
+            citySelect.innerHTML += '<option>' + city + '</option>'
+    
+        })
+    }
 
-    })
 })
